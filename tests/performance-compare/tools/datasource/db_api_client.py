3fa9f7a938435495b7eb83721ddcf608d8bcc5e5
shuai.li <shuai.li@kyligence.io>
2022-10-24 20:04:18 +0800

    add get performance data

---
+++ tests/performance-compare/tools/datasource/db_api_client.py
@@ -0,0 +1,66 @@
+import os
+import time
+from abc import abstractmethod, ABCMeta
+
+from common import config
+
+
+def init_stmt(root_path):
+    stmts = {}
+    for file_name in os.listdir(root_path):
+        full_path = os.path.join(root_path, file_name)
+        if os.path.isfile(full_path) and file_name.endswith(".sql"):
+            with open(full_path, "r") as file:
+                stmts[file_name] = file.read()
+
+    return stmts
+
+
+class DBApiClient(metaclass=ABCMeta):
+    def __init__(self, environment):
+        self.env = environment
+        self.stmt = init_stmt(os.path.join(config.DIALECT_ROOT_PATH, config.DEFAULT_DIALECT))
+        other = self.overwrite_stmt()
+        for k in other:
+            if self.stmt[k]:
+                self.stmt[k] = other[k]
+        self.connection = self.create_connection()
+
+    @abstractmethod
+    def create_connection(self):
+        pass
+
+    def query(self):
+        for name in self.stmt:
+            self.locust_statistic(name, self.stmt[name])
+
+    def locust_statistic(self, name, stmt):
+        request_meta = {
+            "request_type": "python",
+            "name": name,
+            "start_time": time.time(),
+            "response_length": 0,
+            "exception": None,
+            "context": None,
+            "response": None,
+        }
+        start_perf_counter = time.perf_counter()
+        try:
+            request_meta["response"] = self.execute(stmt)
+            request_meta["response_length"] = len(request_meta["response"])
+        except Exception as e:
+            print(e)
+            request_meta["exception"] = e
+        request_meta["response_time"] = int((time.perf_counter() - start_perf_counter) * 1000)
+        self.env.events.request.fire(**request_meta)
+        return request_meta["response"]
+
+    def execute(self, stmt):
+        cursor = self.connection.cursor()
+        cursor.execute(stmt)
+        cursor.close()
+        return "success"
+
+    @abstractmethod
+    def overwrite_stmt(self):
+        pass
---
+++ tests/performance-compare/tools/test.py
@@ -0,0 +1,37 @@
+import argparse
+import os
+import sys
+
+from common import config
+from core import locust_test
+
+parser = argparse.ArgumentParser(description='command line arguments')
+parser.add_argument('--iterations', '-i', type=int,
+                    help='Iteration limit , stops Locust after a certain number of task iterations', required=False)
+parser.add_argument('--output-file', '-o', type=str, help='', required=False)
+parser.add_argument('--engine', "-e", type=str, help='engine', required=False)
+parser.add_argument('--dialect-path', type=str, help='dialect root path', required=False)
+parser.add_argument('--host', type=str, help='host', required=False,
+                    default=os.getenv("TEST_CONNECTION_HOST", "localhost"))
+parser.add_argument('--port', '-p', type=int, help='port', required=False,
+                    default=os.getenv("TEST_CONNECTION_PORT", 80))
+parser.add_argument('--user', '-u', type=str, help='user', required=False,
+                    default=os.getenv("TEST_CONNECTION_USER", "default"))
+parser.add_argument('--password', type=str, help='password', required=False,
+                    default=os.getenv("TEST_CONNECTION_PASSWORD", ""))
+parser.add_argument('--database', "-d", type=str, help='database', required=False,
+                    default=os.getenv("TEST_CONNECTION_DATABASE", "default"))
+
+if __name__ == "__main__":
+    args = vars(parser.parse_args())
+    sys.argv = [sys.argv[0]]
+    config.ITERATIONS = args["iterations"]
+    config.OUTPUT_FILE = args["output_file"]
+    config.DIALECT_ROOT_PATH = args["dialect_path"]
+    config.CONNECTION_HOST = args["host"]
+    config.CONNECTION_PORT = args["port"]
+    config.CONNECTION_USER = args["user"]
+    config.CONNECTION_PASSWORD = args["password"]
+    config.CONNECTION_DATABASE = args["database"]
+    config.ENGINE = args["engine"]
+    locust_test.run()
