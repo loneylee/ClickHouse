dc4468a5f78bf6314bd107528612cf42b1d68acc
Shuai.li <loneylee@live.cn>
2022-11-02 12:20:12 +0800

    add starrocks connection

---
+++ tests/performance-compare/tools/datasource/spark.py
@@ -0,0 +1,26 @@
+from pyhive import hive
+
+from common import config
+from datasource import db_api_client
+
+SQL_DIALECT = "spark"
+
+
+class SparkDBApiClient(db_api_client.DBApiClient):
+
+    def __init__(self, environment):
+        super().__init__(environment)
+
+    def create_connection(self):
+        if config.CONNECTION_PASSWORD == "":
+            return hive.Connection(database=config.CONNECTION_DATABASE, username=config.CONNECTION_USER,
+                                   host=config.CONNECTION_HOST,
+                                   port=config.CONNECTION_PORT)
+        else:
+            return hive.Connection(database=config.CONNECTION_DATABASE, username=config.CONNECTION_USER,
+                                   password=config.CONNECTION_PASSWORD,
+                                   host=config.CONNECTION_HOST,
+                                   port=config.CONNECTION_PORT)
+
+    def get_dialect(self):
+        return SQL_DIALECT
