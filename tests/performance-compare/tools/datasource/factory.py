bc3724337a9cf03402b9595e3a52893cb20c3f21
Shuai.li <loneylee@live.cn>
2022-11-04 19:58:26 +0800

    add starrocks create table

--- tests/performance-compare/tools/datasource/factory.py
+++ tests/performance-compare/tools/datasource/factory.py
@@ -3,18 +3,24 @@ import sys
 
 from common import config
 from datasource import clickhouse
+from datasource import gluten
 from datasource import hive
 from datasource import mysql
+from datasource import starrocks
 
 
 def get_client(environment):
-    client = str.lower(config.CLIENT)
-    if client == str.lower(clickhouse.client):
+    client = str.lower(config.ENGINE)
+    if client == str.lower(clickhouse.ENGINE):
         return clickhouse.ClickhouseDBApiClient(environment)
-    elif client == str.lower(hive.client):
+    elif client == str.lower(hive.ENGINE):
         return hive.HiveDBApiClient(environment)
-    elif client == str.lower(mysql.client):
+    elif client == str.lower(mysql.ENGINE):
         return mysql.MysqlDBApiClient(environment)
+    elif client == str.lower(gluten.ENGINE):
+        return gluten.GlutenDBApiClient(environment)
+    elif client == str.lower(starrocks.ENGINE):
+        return starrocks.StarrocksDBApiClient(environment)
     else:
         logging.getLogger("Engine").error("Engine {} not support", client)
         sys.exit(10)
