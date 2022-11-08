cf01340f778306797ed07be9281a80c85fdf3160
lhuang09287750 <2559010695@qq.com>
2022-11-08 17:08:09 +0800

    add vanilla

---
+++ tests/performance-compare/scripts_ci/services/cleanVanillaSpark.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ $# -ne 1 ];then
+        echo "Usage: ./cleanVanillaSpark.sh key_file"
+        exit 1
+fi
+
+key_file=$1
+
+echo "$(date '+%F %T'): VanillaSpark clean work start!"
+ansible --key-file ${key_file} driver -m shell -a "jps|awk '{print \$1}'|xargs kill -9"
+ansible --key-file ${key_file} workers -m shell -a "jps|grep -i worker|awk '{print \$1}'|xargs kill -9"
+ansible --key-file ${key_file} driver -m shell -a "ps -ef|grep test.py|grep -v grep|awk '{print \$2}'|xargs kill -9"
+
+ansible --key-file ${key_file} tcluster -m shell -a "ps -ef|grep java|grep -v grep"
+sevice_exists=$?
+ansible --key-file ${key_file} tcluster -m shell -a "ps -ef|grep test.py|grep -v grep"
+locust_exists=$?
+
+if [ ${sevice_exists} -ne 0 ] && [ ${locust_exists} -ne 0 ];then
+	echo "$(date '+%F %T'): VanillaSpark clean work done!"
+	exit 0
+else
+	echo "$(date '+%F %T'): VanillaSpark clean work not done!"
+	exit 103
+fi
