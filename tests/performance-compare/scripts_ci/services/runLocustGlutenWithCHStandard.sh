2a18f4a12dd328fd5ee30ed0406cafb96c4df0fa
lhuang09287750 <2559010695@qq.com>
2022-11-25 15:35:09 +0800

    add gluten inner/external switch and write a quick start

--- tests/performance-compare/scripts_ci/services/runLocustGlutenWithCHStandard.sh
+++ tests/performance-compare/scripts_ci/services/runLocustGlutenWithCHStandard.sh
@@ -15,21 +15,20 @@ spark_home=$7
 
 #call locust script,tbd
 echo "$(date '+%F %T'): call call locust script"
-<< COMMENT
-# starrocks create table 2 and query
-echo "$(date '+%F %T'): GlutenWithCHStandard create external table and make query test"
-python3 ${locust_home}/statistic.py --iterations ${iteration} --sql-path ${sqls_home} --dialect gluten --engine gluten --output-dir ${result_dir} \
+if [ ${inner_table} -ne 1 ];then
+  # starrocks create table 2 and query
+  echo "$(date '+%F %T'): GlutenWithCHStandard create external table and make query test"
+  python3 ${locust_home}/statistic.py --iterations ${iteration} --sql-path ${sqls_home} --dialect gluten --engine gluten --output-dir ${result_dir} \
         -p 10000 --host ${private_driver_host} --user root --database tpch100_external --external-path hdfs://${private_namenode_ip}:8020/tmp/tpch-data-sf100 \
         --data-format parquet --drop-table-before-create True
-COMMENT
+fi
 
-#<< COMMENT
+if [ ${inner_table} -eq 1 ];then
 echo "$(date '+%F %T'): GlutenWithCHStandard create inner table"
 python3 ${locust_home}/statistic.py --iterations ${iteration} --sql-path ${sqls_home} --dialect gluten --engine gluten --output-dir ${result_dir}"_inner" \
         -p 10000 --host ${private_driver_host} --user root --database tpch100 --external-path ${data_home}/tpch100_ch_data \
         --data-format mergetree --create-table-only True --drop-table-before-create True --column-nullable ${column_nullable}
 
-<< COMMENT
 echo "$(date '+%F %T'): GlutenWithCHStandard copy metadata to all workers"
 
 for table in $(ls -A ${data_home}/tpch100_ch_data)
@@ -42,4 +41,5 @@ echo "$(date '+%F %T'): GlutenWithCHStandard make query test"
 python3 ${locust_home}/statistic.py --iterations ${iteration} --sql-path ${sqls_home} --dialect gluten --engine gluten --output-dir ${result_dir}"_inner" \
         -p 10000 --host ${private_driver_host} --user root --database tpch100 --external-path ${data_home}/tpch100_ch_data \
         --data-format mergetree --column-nullable ${column_nullable}
-COMMENT
+
+fi
