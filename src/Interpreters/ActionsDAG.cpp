c7eeb7ad5d1ce524f38787ec84377bb74f6ad8be
liuneng <1398775315@qq.com>
2023-04-23 15:07:11 +0800

    change for complex type from liyang
    (cherry picked from commit 70c7f84d620c049d52aab9e57c6b0304faeae8d6)
    (cherry picked from commit dcb685b62af0fe995e1d4dc8fe00063cc94631be)
    (cherry picked from commit d475cfc774eb8def42f4316a5a2ed566ef8832af)
    (cherry picked from commit 1d25fa33228e8d30039151bc0b8738995a77271c)
    (cherry picked from commit 2601d858f3b4cc3b2f8400048712483857fde44d)
    (cherry picked from commit 602b5866278efea2b9f49a27f68270f338d5a1fd)
    (cherry picked from commit 80a8cf474ac4b2078df5fa62d246da1babd0b1ba)
    (cherry picked from commit a66050c3d7792d623397499766291378289a7590)
    (cherry picked from commit 3fe1456855bce1caccff13a0e01c054580042157)
    (cherry picked from commit ebac9d2f4bc25a9ff052133b9b24396104a7ccd9)
    (cherry picked from commit bbe3b35dd58c2c65c26848d3760d0a57788ba8d7)
    (cherry picked from commit d572b7e84da7f9505dd433a1bb5252d4266c4169)
    (cherry picked from commit 660f80896eb228acaed83478603373189b9f8c6d)
    (cherry picked from commit f1a7b601879613199a093135ce599cb2b60677e2)

--- src/Interpreters/ActionsDAG.cpp
+++ src/Interpreters/ActionsDAG.cpp
@@ -242,6 +242,7 @@ const ActionsDAG::Node & ActionsDAG::addFunction(
 
     auto function_base = function->build(arguments);
     return addFunctionImpl(
+        function,
         function_base,
         std::move(children),
         std::move(arguments),
@@ -258,6 +259,7 @@ const ActionsDAG::Node & ActionsDAG::addFunction(
     auto [arguments, all_const] = getFunctionArguments(children);
 
     return addFunctionImpl(
+        {},
         function.getFunction(),
         std::move(children),
         std::move(arguments),
@@ -274,6 +276,7 @@ const ActionsDAG::Node & ActionsDAG::addFunction(
     auto [arguments, all_const] = getFunctionArguments(children);
 
     return addFunctionImpl(
+        {},
         function_base,
         std::move(children),
         std::move(arguments),
@@ -299,6 +302,7 @@ const ActionsDAG::Node & ActionsDAG::addCast(const Node & node_to_cast, const Da
 }
 
 const ActionsDAG::Node & ActionsDAG::addFunctionImpl(
+    const FunctionOverloadResolverPtr & function,
     const FunctionBasePtr & function_base,
     NodeRawConstPtrs children,
     ColumnsWithTypeAndName arguments,
@@ -315,6 +319,7 @@ const ActionsDAG::Node & ActionsDAG::addFunctionImpl(
     node.function_base = function_base;
     node.result_type = result_type;
     node.function = node.function_base->prepare(arguments);
+    node.function->setResolver(function); /// It's needed for spark compatibility.
     node.is_deterministic = node.function_base->isDeterministic();
 
     /// If all arguments are constants, and function is suitable to be executed in 'prepare' stage - execute function.
@@ -2910,6 +2915,7 @@ ActionsDAGPtr ActionsDAG::buildFilterActionsDAG(
                 auto function_base = function_overload_resolver ? function_overload_resolver->build(arguments) : node->function_base;
 
                 result_node = &result_dag->addFunctionImpl(
+                    function_overload_resolver,
                     function_base,
                     std::move(function_children),
                     std::move(arguments),
