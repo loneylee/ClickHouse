8af8523a1f41c555628bfe5598cbacd20dd9d56d
李扬 <654010905@qq.com>
2023-07-20 10:15:37 +0800

    [GLUTEN-1914] Allow nullable complex type when convert arrow column to ch column.  (#450)
    (cherry picked from commit 1f2ae61672e620e0541219710ca3e7a94c3b718f)

--- src/Processors/Formats/Impl/ArrowColumnToCHColumn.cpp
+++ src/Processors/Formats/Impl/ArrowColumnToCHColumn.cpp
@@ -1134,10 +1134,7 @@ static ColumnWithTypeAndName readColumnFromArrowColumn(
 {
     bool read_as_nullable_column = (arrow_column->null_count() || is_nullable_column || (type_hint && type_hint->isNullable())) && settings.allow_inferring_nullable_columns;
     if (read_as_nullable_column &&
-        arrow_column->type()->id() != arrow::Type::LIST &&
         arrow_column->type()->id() != arrow::Type::LARGE_LIST &&
-        arrow_column->type()->id() != arrow::Type::MAP &&
-        arrow_column->type()->id() != arrow::Type::STRUCT &&
         arrow_column->type()->id() != arrow::Type::DICTIONARY)
     {
         DataTypePtr nested_type_hint;
