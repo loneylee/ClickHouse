697bec16ef98719aa5dbe2c833353a9e7fddd2d4
Chang Chen <baibaichen@gmail.com>
2024-05-26 09:55:58 +0800

    make HDFSObjectStorage members protected (#485)
    (cherry picked from commit b94d0349e2c767daf254ec2e4e786de8e4200c7c)

--- src/Disks/ObjectStorages/HDFS/HDFSObjectStorage.h
+++ src/Disks/ObjectStorages/HDFS/HDFSObjectStorage.h
@@ -113,7 +113,7 @@ public:
 
     void shutdown() override { }
 
-private:
+protected:
     void initializeHDFSFS() const;
     std::string extractObjectKeyFromURL(const StoredObject & object) const;
 
