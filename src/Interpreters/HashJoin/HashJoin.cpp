c778d35a03da8a2ca317aa7a5218486f33d9caa7
lgbo <lgbo.ustc@gmail.com>
2024-09-03 10:26:05 +0800

    [GLUTEN-7077] `have_compressed` is lost in `HashJoin::reuseJoinedData` (#502)

--- src/Interpreters/HashJoin/HashJoin.cpp
+++ src/Interpreters/HashJoin/HashJoin.cpp
@@ -1394,6 +1394,7 @@ void HashJoin::reuseJoinedData(const HashJoin & join)
     have_compressed = join.have_compressed;
     data = join.data;
     from_storage_join = true;
+    have_compressed = join.have_compressed;
 
     bool flag_per_row = needUsedFlagsForPerRightTableRow(table_join);
     if (flag_per_row)
